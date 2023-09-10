6274a22929fb2d62eac200f361a3b9fecfe68643
raphael meyer <raphael.akira.meyer@gmail.com>
2023-09-10 12:20:35 +0200

    PoC for accessing alsa sequencer by using cgo

--- trequer/trequer/trequer.go
+++ trequer/trequer/trequer.go
@@ -1,5 +1,9 @@
 package trequer
 
+import (
+	"github.com/raphaelmeyer/trequer/midi"
+)
+
 type MidiPort struct {
 	Id     string `json:"id"`
 	Client string `json:"client"`
@@ -7,18 +11,23 @@ type MidiPort struct {
 }
 
 type Trequer struct {
-	ports []MidiPort
 }
 
 func NewTrequer() *Trequer {
-	return &Trequer{
-		ports: []MidiPort{
-			{Id: "14:0", Client: "Midi Through", Port: "Midi Through Port-0"},
-			{Id: "128:0", Client: "FLUID Synth (1307)", Port: "Synth input port (1307:0)"},
-		},
-	}
+	return &Trequer{}
 }
 
 func (t *Trequer) GetMidiPorts() []MidiPort {
-	return t.ports
+	seq := midi.NewSequencer()
+
+	ports := []MidiPort{}
+	for _, port := range seq.ListPorts() {
+		ports = append(ports, MidiPort{
+			Id:     port.Id,
+			Client: port.Client,
+			Port:   port.Port,
+		})
+	}
+
+	return ports
 }
