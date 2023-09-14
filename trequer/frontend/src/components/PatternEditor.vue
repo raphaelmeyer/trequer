bccd871cb2b9244a4bb7e6a6d523161820854a10
raphael meyer <raphael.akira.meyer@gmail.com>
2023-09-14 23:44:37 +0200

    Experiment with track layout

--- trequer/frontend/src/components/PatternEditor.vue
+++ trequer/frontend/src/components/PatternEditor.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
-import { keyName } from '@/models/midi';
+import type { Note } from '@/models/song';
+import TrackRow from '@/components/TrackRow.vue';
 
-const pattern = Array.from({ length: 64 }, () => '... ...');
+const notes: Note[] = [
+  { tick: 0, key: 57, volume: 64, length: 2 },
+  { tick: 4, key: 64, volume: 64, length: 2 },
+  { tick: 8, key: 72, volume: 64, length: 2 },
+  { tick: 12, key: 64, volume: 64, length: 2 },
 
-pattern[0] = keyName(69);
-pattern[4] = keyName(73);
-pattern[8] = keyName(21);
-pattern[16] = keyName(127);
+  { tick: 16, key: 57, volume: 64, length: 2 },
+  { tick: 20, key: 64, volume: 64, length: 2 },
+  { tick: 24, key: 72, volume: 64, length: 2 },
+  { tick: 28, key: 64, volume: 64, length: 2 },
+
+  { tick: 32, key: 57, volume: 64, length: 2 },
+  { tick: 36, key: 64, volume: 64, length: 2 },
+  { tick: 40, key: 72, volume: 64, length: 2 },
+  { tick: 44, key: 64, volume: 64, length: 2 },
+
+  { tick: 48, key: 57, volume: 64, length: 2 },
+  { tick: 52, key: 64, volume: 64, length: 2 },
+  { tick: 56, key: 72, volume: 64, length: 2 },
+  { tick: 60, key: 64, volume: 64, length: 2 },
+];
 </script>
 
 <template>
-  <div v-for="(note, index) in pattern" :key="index">{{ note }}</div>
+  <div>
+    <TrackRow v-for="(note, index) in notes" :key="index" :note="note" />
+  </div>
 </template>
---
+++ trequer/frontend/src/components/TrackRow.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { keyName } from '@/models/midi';
+import type { Note } from '@/models/song';
+
+const props = defineProps<{
+  note: Note;
+}>();
+</script>
+
+<template>
+  <div class="flex bg-sky-950 w-32 px-2">
+    <div class="w-10">{{ keyName(props.note.key) }}</div>
+    <div class="w-8">{{ props.note.volume }}</div>
+    <div class="w-8">{{ props.note.length }}</div>
+  </div>
+</template>
