6cfa634b1e056bf8ab311b156e03115186ceb7f1
raphael meyer <raphael.akira.meyer@gmail.com>
2023-09-14 21:54:18 +0200

    Set up test example

--- trequer/frontend/src/models/midi.ts
+++ trequer/frontend/src/models/midi.ts
@@ -2,10 +2,10 @@ const names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 
 export function keyName(key: number): string {
   if (key < 21) {
-    return '---';
+    return '';
   }
   if (key > 127) {
-    return '---';
+    return '';
   }
 
   const note = key % 12;
