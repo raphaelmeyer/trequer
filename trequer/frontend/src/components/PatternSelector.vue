30f8593a1d54aa2e3ce7ef12ac97b6ae0f4405d1
raphael meyer <raphael.akira.meyer@gmail.com>
2023-09-17 20:24:27 +0200

    Add skeleton for pattern selector

---
+++ trequer/frontend/src/components/PatternSelector.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { mdiArrowLeftCircle, mdiArrowRightCircle } from '@mdi/js';
+
+const pattern = 0;
+</script>
+
+<template>
+  <div class="flex justify-between gap-2 bg-sky-950 border-sky-600 border-4 rounded-xl p-1 w-32">
+    <svg class="w-6 h-6" viewBox="0 0 24 24">
+      <path :d="mdiArrowLeftCircle" class="fill-sky-200" />
+    </svg>
+    <div class="font-bold">{{ pattern }}</div>
+    <svg class="w-6 h-6" viewBox="0 0 24 24">
+      <path :d="mdiArrowRightCircle" class="fill-sky-200" />
+    </svg>
+  </div>
+</template>
--- trequer/frontend/src/views/TrackerView.vue
+++ trequer/frontend/src/views/TrackerView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import SelectOutput from '@/components/SelectOutput.vue';
 import PatternEditor from '@/components/PatternEditor.vue';
+import PatternSelector from '@/components/PatternSelector.vue';
 </script>
 
 <template>
   <div class="p-2">
+    <PatternSelector></PatternSelector>
     <SelectOutput></SelectOutput>
     <PatternEditor></PatternEditor>
   </div>
