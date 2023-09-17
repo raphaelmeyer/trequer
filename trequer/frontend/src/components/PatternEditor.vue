43a0fb83defdf272ff245058fb1f205deb753fdf
raphael meyer <raphael.akira.meyer@gmail.com>
2023-09-18 01:36:25 +0200

    Add more channels to layout sketch

--- trequer/frontend/src/components/PatternEditor.vue
+++ trequer/frontend/src/components/PatternEditor.vue
@@ -5,12 +5,24 @@ import LabelBox from './LabelBox.vue';
 
 const song = useSongStore();
 const track = song.track(0, 0);
+
+const channels = [0, 1, 2, 3, 4];
+function channelName(channel: number): string {
+  return `Channel ${channel}`;
+}
 </script>
 
 <template>
-  <LabelBox label="Channel 0" class="w-32">
-    <div class="flex flex-col">
-      <TrackRow v-for="(note, index) in track" :key="index" :note="note" />
-    </div>
-  </LabelBox>
+  <div class="flex gap-x-2">
+    <LabelBox
+      v-for="(channel, index) in channels"
+      :key="index"
+      :label="channelName(channel)"
+      class="w-32"
+    >
+      <div class="flex flex-col">
+        <TrackRow v-for="(note, noteIndex) in track" :key="noteIndex" :note="note" />
+      </div>
+    </LabelBox>
+  </div>
 </template>
