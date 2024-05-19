1d588834ab04317886b4d6cdf5b0a77e3d31ced0
Raphael Meyer <raphael.akira.meyer@gmail.com>
2024-05-19 13:31:19 +0200

    Clean up generated frontend project

--- trequer/frontend/src/router/index.ts
+++ trequer/frontend/src/router/index.ts
@@ -1,23 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import TrequerView from '../views/TrequerView.vue';
+import SettingsView from '../views/SettingsView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'trequer',
+      component: TrequerView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/settings',
+      name: 'settings',
+      component: SettingsView
     }
   ]
-})
+});
 
-export default router
+export default router;
