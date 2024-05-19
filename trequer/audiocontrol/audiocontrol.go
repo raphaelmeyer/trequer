8caf9c811ac167312feca5b2f80919f6ba07b992
Raphael Meyer <raphael.akira.meyer@gmail.com>
2024-05-19 16:01:06 +0200

    Prototype for getting midi ports in frontend

--- trequer/audiocontrol/audiocontrol.go
+++ trequer/audiocontrol/audiocontrol.go
@@ -11,6 +11,7 @@ jack_client_t * jack_client_open_ (const char *client_name, jack_options_t optio
 */
 import "C"
 import (
+	"fmt"
 	"unsafe"
 )
 
@@ -21,9 +22,38 @@ type AudioControl struct {
 func NewAudioControl() *AudioControl {
 	ac := new(AudioControl)
 
-	clientName := C.CString("trequer")
+	clientName := C.CString("Trequer")
 	defer C.free(unsafe.Pointer(clientName))
 	ac.client = C.jack_client_open_(clientName, C.JackNullOption, nil)
 
 	return ac
+}
+
+func (ac *AudioControl) ListMidiOutputs() []string {
+	filter := C.CString("midi")
+	defer C.free(unsafe.Pointer(filter))
+
+	var ports []string
+
+	cports, _ := C.jack_get_ports(ac.client, nil, filter, C.JackPortIsInput)
+	if cports != nil {
+		defer C.jack_free(unsafe.Pointer(cports))
+		cport := cports
+		for *cport != nil {
+			name := C.GoString(*cport)
+
+			fmt.Println(name)
+			ports = append(ports, name)
+
+			cport = (**C.char)(unsafe.Add(unsafe.Pointer(cport), unsafe.Sizeof(cport)))
+		}
+	}
+
+	return ports
+
+	// auto ports = jack_get_ports(client, nullptr, "midi", JackPortIsInput);
+	// for (char const **port = ports; *port != nullptr; ++port) {
+	//   printf("%s\n", *port);
+	// }
+	// jack_free(ports);
 }
--- trequer/frontend/src/views/TrequerView.vue
+++ trequer/frontend/src/views/TrequerView.vue
@@ -1,5 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ListMidiOutputs } from '../../wailsjs/go/audiocontrol/AudioControl';
+
+import { onMounted, ref } from 'vue';
+
+const ports = ref<string[]>([]);
+
+onMounted(() => {
+  ListMidiOutputs().then((value) => {
+    ports.value = value;
+  });
+});
+</script>
 
 <template>
   <div>Trequer</div>
+  <div v-for="port in ports" :key="port">{{ port }}</div>
 </template>
